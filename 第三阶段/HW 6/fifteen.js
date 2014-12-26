46a7a3072fd131ff7cf45e98611812ffe9bb82c5
Valen <wwl0816@qq.com>
2014-12-26 14:44:41 +0800

    Revert "update HW6 with jQuery and add a animate"
    This reverts commit 2d54991f861aba8c55a1b142fe374032390e9f43.

--- 第三阶段/HW 6/fifteen.js
+++ 第三阶段/HW 6/fifteen.js
@@ -4,30 +4,31 @@
  * @Email   : ValenW@qq.com
  * @Date    : 2014-12-13 13:03:50
  * @Last Modified by:   ValenW
- * @Last Modified time: 2014-12-26 14:39:53
+ * @Last Modified time: 2014-12-24 22:43:34
  */
 
-$(document).ready(function() {
-    $("#shufflebutton").click(shuffle);
+window.onload = function() {
+    document.getElementById("shufflebutton").onclick = shuffle;
     wx = wy = 3;
     box = [];
-    pieces = $("#puzzlearea > div");
-    pieces.each(function(i) {
-        this.className = "puzzlepiece";
-        this.style.backgroundPosition = "-" + (i % 4)*100 + "px -" + parseInt(i / 4)*100 + "px";
-        this.style.top = parseInt(i / 4)*100 + "px";
-        this.style.left = (i % 4)*100 + "px";
-        this.bId = i;
-        this.onclick = pieClick;
+    pieces = document.querySelectorAll("#puzzlearea > div");
+    for (var i = 0; i < pieces.length; i++) {
+        pieces[i].className = "puzzlepiece";
+        pieces[i].style.backgroundPosition = "-" + (i % 4)*100 + "px -" + parseInt(i / 4)*100 + "px";
+        pieces[i].style.top = parseInt(i / 4)*100 + "px";
+        pieces[i].style.left = (i % 4)*100 + "px";
+        pieces[i].bId = i;
+        pieces[i].onclick = pieClick;
         box[i] = i;
-    });
-});
+    }
+}
 
 function shuffle() {
-    var t = false;
+    var oldR = 3, t = false;
     var rList = [];
     rList[0] = wy*4 + wx;
-    if ($("#windiv")) $("#windiv").remove();
+    if (document.getElementById("windiv"))
+        document.getElementById("overall").removeChild(document.getElementById("windiv"));
     var diff = prompt('Please enter the random scrambling step (recommended more than 100, 100 by default):', 100);
     if (isNaN(parseInt(diff)) || parseInt(diff) < 0) diff = 100;
     for (var i = 1; i <= diff; i++) {
@@ -45,15 +46,13 @@ function shuffle() {
         rList[i] = y*4 + x;
         t = !t;
     }
-    for (var i = 1; i <= diff; i++) moveP(rList[i], 50);
+    for (var i = 1; i <= diff; i++) moveP(rList[i]);
     update();
 }
 
-function moveP(bid, time) {
-    if (!time) time = 500;
-    $(pieces[box[bid]]).animate({
-        top: wy*100 + 'px',
-        left: wx*100 + 'px'}, time);
+function moveP(bid) {
+    pieces[box[bid]].style.top = wy*100 + 'px';
+    pieces[box[bid]].style.left = wx*100 + 'px';
     pieces[box[bid]].bId = wy*4 + wx;
     box[wy*4 + wx] = box[bid];
     wx = bid % 4;
@@ -65,13 +64,13 @@ function update() {
     for(var i = 0; i < 4; i++)
         for (var j = 0; j < 4; j++)
             if ((Math.abs(i - wx) + Math.abs(j - wy)) == 1)
-                $(pieces[box[i+j*4]]).addClass("movablepiece");
+                pieces[box[i+j*4]].className = "puzzlepiece movablepiece";
             else if (box[i+j*4] != -1)
-                $(pieces[box[i+j*4]]).removeClass("movablepiece");
+                pieces[box[i+j*4]].className = "puzzlepiece";
 }
 
 function pieClick() {
-    if ($(this).is(".movablepiece")) {
+    if (this.className == "puzzlepiece movablepiece") {
         moveP(this.bId);
         var i = 0;
         for (i = 0; i < 15 && box[i] == i; i++);
@@ -81,9 +80,12 @@ function pieClick() {
 }
 
 function win() {
-    $(".puzzlepiece").each(function() {$(this).removeClass("movablepiece")});
-    var windiv = $("<div></div>").html(
-        "You Win!<br>But you are not the BEST!<br>Becouse...<br>" +
-        "<img src='Pml.jpg' width='288' height='480'>").attr("id", "windiv");
-    $("#puzzlearea").before(windiv);
+    for (i = 0; i < 15; i++) pieces[i].className = "puzzlepiece";
+    var windiv = document.createElement("div");
+    windiv.innerHTML = "You Win!<br>" +
+                       "But you are not the BEST!<br>" +
+                       "Becouse...<br>" +
+                       "<img src='Pml.jpg' width='288' height='480'>";
+    windiv.id = "windiv";
+    document.getElementById("overall").insertBefore(windiv, document.getElementById("puzzlearea"));
 }
