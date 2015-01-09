f5627d7886bdd7f5d93f25f429f0dad40a5af25b
Valen <wwl0816@qq.com>
2015-01-09 22:17:20 +0800

    实现中文注册名

--- FinalTest/1/static/js/index.js
+++ FinalTest/1/static/js/index.js
@@ -5,7 +5,7 @@
  * @Email   : ValenW@qq.com
  * @Date    : 2015-01-06 20:04:37
  * @Last Modified by:   ValenW
- * @Last Modified time: 2015-01-09 21:24:51
+ * @Last Modified time: 2015-01-09 22:12:46
  */
 $(document).ready(function() {
     $("#bs-example-navbar-collapse-1 li").click(function (e) {
@@ -35,7 +35,7 @@ $(document).ready(function() {
         });
     });
     $("#signupBtn").click(function() {
-        var re = /^\w{2,15}$/
+        var re = /^[\w\u4E00-\u9FA5\uF900-\uFA2D]{2,15}$/
         if (re.test($("#name").val()) && re.test($("#password").val())) {
             $.ajax({
                 type: "POST",
